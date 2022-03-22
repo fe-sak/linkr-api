b13e566290da795141d5b5b9a4158366a3ec014c
Francisca <fraan928@gmail.com>
2022-03-22 13:43:18 -0300

    User authentication with JWT

--- src/validations/userSchema.js
+++ src/validations/userSchema.js
@@ -7,6 +7,12 @@ const signUpSchema = joi.object({
     pictureUrl: joi.string().pattern(/https?:\/\/.*.(?:png|jpg)/),
 });
 
+const loginSchema = joi.object({
+    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: joi.string().min(6),
+});
+
 export {
     signUpSchema,
+    loginSchema,
 };
