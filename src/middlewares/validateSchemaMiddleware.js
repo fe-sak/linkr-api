f1df10ce08add903194fa7f9febd1ee38c40c613
Felipe Sakuma <fycorsak@gmail.com>
2022-03-22 16:29:50 -0300

    change in validateSchemaMiddleware

--- src/middlewares/validateSchemaMiddleware.js
+++ src/middlewares/validateSchemaMiddleware.js
@@ -1,10 +1,16 @@
 export function validateSchemaMiddleware(schema) {
-    return (req, res, next) => { 
-        const validation = schema.validate(req.body);
-        if (validation.error) {
-            return res.sendStatus(422);
-        }
-        
-        next();
+  return (req, res, next) => {
+    const validation = schema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+      return res
+        .status(422)
+        .send(
+          validation.error.details.map((detail) =>
+            detail.message.replace(/"/g, '')
+          )
+        );
     }
+
+    next();
+  };
 }
