c73fa6a811bf463e80c7590f6fb5fa41d6c2500c
Felipe Sakuma <fycorsak@gmail.com>
2022-03-23 14:14:49 -0300

    chore: createLinkSnippet function

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import postsRouter from './postsRouter.js';
 import userRouter from './userRouter.js';
 
 const router = Router();
-router.use(postsRouter)
 
 router.use(userRouter);
 router.use(postsRouter);
