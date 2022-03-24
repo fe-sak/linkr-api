f9382c46983515de6530f3cfaf445bfd265e7f25
Felipe Sakuma <fycorsak@gmail.com>
2022-03-24 20:29:33 -0300

    feat: posts likes

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -5,16 +5,21 @@ async function read() {
   SELECT
     posts.id,
     posts.comment,
+    posts.user_id AS "user_id",
     users.username,
     users.picture_url AS "userPic",
     links.title AS "linkTitle",
     links.image AS "linkImage",
     links.description AS "linkDescription",
-    links.url AS url
+    links.url AS url,
+    likeS.id AS like_id,
+    likes.user_id AS like_user_id,
+    likes.post_id AS like_post_id
   FROM
     posts
     JOIN users ON posts.user_id = users.id
     JOIN links ON posts.link_id = links.id
+    JOIN likes ON posts.id = likes.post_id
   ORDER BY
     timestamp DESC
   LIMIT
--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,12 +1,11 @@
-import { Router } from "express";
-import { postPosts, readPosts } from "../controllers/postsController.js";
-import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
-import postSchema from "../schemas/postSchema.js";
-import { auth } from '../middlewares/authMiddleware.js';
+import { Router } from 'express';
+import { postPosts, readPosts } from '../controllers/postsController.js';
+import { validateSchemaMiddleware } from '../middlewares/validateSchemaMiddleware.js';
+import postSchema from '../schemas/postSchema.js';
 
 const postsRouter = Router();
 
-postsRouter.get('/posts', auth, readPosts);
+postsRouter.get('/posts', readPosts);
 
 postsRouter.post('/posts', validateSchemaMiddleware(postSchema), postPosts); //missing token
 
