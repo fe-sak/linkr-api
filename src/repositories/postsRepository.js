fcbe30253d56f0e6f4dec3b3233428a8e7d05836
Felipe Sakuma <fycorsak@gmail.com>
2022-03-28 10:34:53 -0300

    fix: fix sql query in hashtagTrending

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -30,10 +30,13 @@ async function read() {
 }
 
 async function findById({ postId }) {
-  const post = await connection.query(`
+  const post = await connection.query(
+    `
       SELECT * FROM posts
       WHERE id = $1;
-  `, [postId]);
+  `,
+    [postId]
+  );
 
   return post.rows[0];
 }
@@ -107,7 +110,12 @@ async function insertHashtag(name) {
   return result;
 }
 async function hashtagTrending() {
-  const { rows: hashtags } = await connection.query(`SELECT * FROM hashtags;`);
+  const { rows: hashtags } = await connection.query(`
+    SELECT
+      *
+    FROM
+      hashtags_posts
+      JOIN hashtags ON hashtags.id = hashtags_posts.hashtag_id`);
   return hashtags;
 }
 
@@ -122,7 +130,8 @@ async function deleteById(id) {
 }
 
 async function getPostByUser(id) {
-  const result = await connection.query(`
+  const result = await connection.query(
+    `
    SELECT
     posts.id,
     posts.comment,
@@ -147,44 +156,57 @@ async function getPostByUser(id) {
   ORDER BY
     posts.id DESC
   LIMIT
-    20`, [id]);
+    20`,
+    [id]
+  );
 
-  return result
+  return result;
 }
 
 async function getPostByHashtag(name) {
   const { rows: posts } = await connection.query(
-    `SELECT
-    posts.id,
-    posts.comment,
-    users.username,
-    users.picture_url AS "userPic",
-    links.title AS "linkTitle",
-    links.image AS "linkImage",
-    links.description AS "linkDescription",
-    links.url AS url
-  FROM
-    posts
-    JOIN users ON posts.user_id = users.id
-    JOIN links ON posts.link_id = links.id
-    JOIN hashtags_posts hp ON posts.id=hp.post_id
-    JOIN hashtags h ON hp.hashtag_id=h.id
-    WHERE h.name=$1`,
+    `
+      SELECT
+        posts.id,
+        posts.comment,
+        posts.user_id AS "userId",
+        usersP.username,
+        usersP.picture_url AS "userPic",
+        links.title AS "linkTitle",
+        links.image AS "linkImage",
+        links.description AS "linkDescription",
+        links.url AS url,
+        likeS.id AS "likeId",
+        likes.user_id AS "likeUserId",
+        usersL.username AS "likeUsername"
+      FROM
+        posts
+        JOIN users usersP ON posts.user_id = usersP.id
+        JOIN links ON posts.link_id = links.id
+        LEFT JOIN likes ON posts.id = likes.post_id
+        LEFT JOIN users usersL ON likes.user_id=usersL.id
+        JOIN hashtags_posts hp ON posts.id=hp.post_id
+        JOIN hashtags h ON hp.hashtag_id=h.id
+      WHERE h.name=$1
+      ORDER BY
+        posts.id DESC
+      LIMIT
+        20
+      `,
     [name]
   );
   return posts;
 }
 
-async function updateComment(comment, postId, userId){
+async function updateComment(comment, postId, userId) {
   await connection.query(
     `UPDATE posts SET comment=$1 WHERE id=$2 AND user_id=$3`,
-    [comment, postId, userId]);
+    [comment, postId, userId]
+  );
 }
 
-async function deleteHashtagPostItem(id){
-  await connection.query(
-    `DELETE FROM hashtags_posts WHERE post_id=$1`, [id]
-  );
+async function deleteHashtagPostItem(id) {
+  await connection.query(`DELETE FROM hashtags_posts WHERE post_id=$1`, [id]);
 }
 
 export {
